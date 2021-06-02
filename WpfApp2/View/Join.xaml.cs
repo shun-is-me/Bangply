c75f8b87dbdaa83e983822312fd12acb30b4c0ec
shun-is-me <rhkrtlgus@naver.com>
2021-06-03 03:07:02 +0900

    로그인 조인 데이터베이스 연동

--- WpfApp2/View/Join.xaml.cs
+++ WpfApp2/View/Join.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,9 +30,54 @@ namespace WpfApp2.View
             NavigationService.Navigate(new Uri("/view/Home.xaml", UriKind.Relative));
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)//회원가입 -- 로그인
         {
-            NavigationService.Navigate(new Uri("/view/Login.xaml", UriKind.Relative));
+
+
+            //모든정보입력하도록
+            if (string.IsNullOrEmpty(textbox1.Text))
+            {
+                MessageBox.Show("ID를 입력해주세요");
+                this.textbox1.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(textbox2.Text))
+            {
+                MessageBox.Show("PWD을 입력해주세요");
+                this.textbox2.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(textbox3.Text))
+            {
+                MessageBox.Show("닉네임을 입력해주세요");
+                this.textbox3.Focus();
+                return;
+            }
+
+
+            try
+            {
+                MySqlConnection conn = new MySqlConnection("Server=localhost;Database=musicr;UId=root;Password=1234;");
+                MySqlCommand comm = new MySqlCommand();
+                conn.Open();
+
+
+                comm.CommandText = "INSERT INTO USER(uId, uPwd, uNname) VALUES (@uId, @uPwd, @uNname)";
+
+                comm.Parameters.Add("@uId", MySqlDbType.Text).Value = textbox1.Text;
+                comm.Parameters.Add("@uPwd", MySqlDbType.Text).Value = textbox2.Text;
+                comm.Parameters.Add("@uNname", MySqlDbType.Text).Value = textbox3.Text;
+              
+
+                comm.Connection = conn;
+
+                comm.ExecuteNonQuery();
+                conn.Close();
+
+                MessageBox.Show("회원가입 되었습니다.");
+                NavigationService.Navigate(new Uri("/view/Login.xaml", UriKind.Relative));
+            }
+            catch (Exception ex) { MessageBox.Show("아이디가 중복 되었습니다."); }
         }
     }
 }
--- WpfApp2/View/Login.xaml.cs
+++ WpfApp2/View/Login.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp2.Model;
 
 namespace WpfApp2.View
 {
@@ -29,9 +32,60 @@ namespace WpfApp2.View
             NavigationService.Navigate(new Uri("/view/Home.xaml", UriKind.Relative));
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)//로그인 버튼 
         {
-            NavigationService.Navigate(new Uri("/view/Choice.xaml", UriKind.Relative));
+            //id password 모두 입력할 시 로그인 되게
+            if (string.IsNullOrEmpty(textbox1.Text))
+            {
+                MessageBox.Show("Id를 입력해주세요");
+                this.textbox1.Focus();
+                return;
+            }
+
+            if (string.IsNullOrEmpty(passwordbox.Password))
+            {
+                MessageBox.Show("password를 입력해주세요");
+                this.passwordbox.Focus();
+                return;
+            }
+
+            string uId = textbox1.Text;
+            string uPwd = passwordbox.Password;
+
+            //user -> list화해서 입력한 아이디와 비밀번호와 일치하는지 
+            string connectionString = "Server=localhost;Database=musicr;UId=root;Password=1234;";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand cmd = new MySqlCommand("select * from user", connection);
+            connection.Open();
+            DataTable dt = new DataTable();
+
+            dt.Load(cmd.ExecuteReader());
+
+            List<user> Users = dt.DataTableToList<user>();
+
+
+            try
+            {
+                user user1 = Users.Single((x) => x.uId.Equals(uId));
+
+
+                if (user1.uPwd.Equals(uPwd))
+                {
+                    Application.Current.Properties["loginID"] = user1.uId;
+                    MessageBox.Show("로그인 되었습니다.");
+                    NavigationService.Navigate(new Uri("/view/Choice.xaml", UriKind.Relative));
+                }
+                else
+                {
+                    MessageBox.Show("비밀번호가 틀렸습니다.");
+                }
+                connection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("존재하지 않는 아이디입니다.");
+            }
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
